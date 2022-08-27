31928f1afbec6ef10d36dd02b94bff0a916937b5
edrissa <edrissasaine366@gmail.com>
2022-08-27 11:26:13 +0000

    implement endpoints

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const middleware = require('./utilities/middleware')
+
+app.use(express.json())
+
+
 const me = {
     fname: "Edrissa",
     lname: "Saine",
@@ -17,6 +22,22 @@ const me = {
     dob: "19/11/199*" 
 }
 
+let users = [
+  {
+    id: 1,
+    username: "idris",
+    phone: "0001111",
+    password: "secrete"
+  }, 
+
+  {
+    id: 2,
+    username: "jankeh",
+    phone: "0001111",
+    password: "secrete"
+  }
+]
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -25,6 +46,54 @@ app.get('/me', (req, res) => {
     res.json(me)
 })
 
+//Get One
+app.get('/api/users/:id', (req, res) => {
+  const id = req.params.id
+  const user = users.find((user) => user.id === Number(id))
+
+  if(user) {
+    res.json(user)
+  }
+  
+  else{
+    res.status(404).end()
+    //res.send("sorry user not found")
+  }
+})
+
+//Get All
+app.get('/api/users', (req, res) => {
+  res.json(users)
+})
+
+//Delete One
+app.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id
+  users = users.filter((user) => user.id !== Number(id))
+
+  res.status(204).end()
+})
+
+app.post('/api/users', (req, res) => {
+  const content = req.body
+
+  //here we save the content to the database
+  console.log(content);
+  res.json(content)
+})
+
+app.put('api/users/:id', (req, res) =>{
+  const id = req.params.id
+
+  res.id
+})
+
+//app.put()
+app.use(middleware.unknownEndpoint)
+app.use(middleware.requestLogger)
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
---
+++ src/utilities/middleware.js
@@ -0,0 +1,21 @@
+
+const requestLogger = (req, res, next) => {
+    console.log('METHOD', req.method)
+    console.log('Path:', req.path)
+    console.log('Body:', req.body)
+}
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({
+        error: 'unknown endpoint'
+    })
+}
+
+module.exports = {
+    requestLogger,
+    unknownEndpoint
+}
+
+
+
+
